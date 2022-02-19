f18248fa8bf8fb0d03bb77fd02e5f011b02a65fa
vr009 <slavarianov@yandex.ru>
2022-02-19 16:32:33 +0300

    daemon mode set

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
+	"log"
 	"net"
 	"proxy/internal"
 )
 
 func main() {
 
-	ln, _ := net.Listen("tcp", ":8080")
-	conn, _ := ln.Accept()
-	defer conn.Close()
+	for {
+		ln, err := net.Listen("tcp", ":8080")
+		if err != nil {
+			log.Println(err)
+		}
+		conn, _ := ln.Accept()
 
-	/* getting request from client */
-	req := internal.GetRequest(conn)
+		/* getting request from client */
+		req := internal.GetRequest(conn)
 
-	if internal.ParsePort(req.Host) != "" {
-		req.Port = ""
+		if internal.ParsePort(req.Host) != "" {
+			req.Port = ""
+		}
+		connTo, err := net.Dial("tcp", req.Host+req.Port)
+		if err != nil {
+			panic(err)
+		}
+
+		/* proxying the message from client to dest */
+		answer := internal.ProxyRequest(connTo, req.FullMsg)
+
+		/* returning response */
+		internal.ReturnResponse(conn, answer)
+
+		conn.Close()
+		ln.Close()
+		ln = nil
+		conn = nil
 	}
-	connTo, err := net.Dial("tcp", req.Host+req.Port)
-	if err != nil {
-		panic(err)
-	}
-
-	/* proxying the message from client to dest */
-	answer := internal.ProxyRequest(connTo, req.FullMsg)
-
-	/* returning response */
-	internal.ReturnResponse(conn, answer)
-	connTo.Close()
 }
