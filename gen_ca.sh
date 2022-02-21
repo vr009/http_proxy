123495a23e5c0afe4c51e9fef10f17a09a00c6a7
vr009 <slavarianov@yandex.ru>
2022-02-21 11:53:58 +0300

    cert scripts fixed

--- gen_ca.sh
+++ gen_ca.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=yngwie proxy CA"
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=vr009 proxy CA"
 openssl genrsa -out cert.key 2048
 mkdir certs/
